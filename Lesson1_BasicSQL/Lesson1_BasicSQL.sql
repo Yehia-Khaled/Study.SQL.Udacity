464cbaef7e092a25b4622d1426edf260393160a0
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2025-01-02 14:09:25 +0200

    1.19 Quiz: ORDER BY

--- Lesson1_BasicSQL/Lesson1_BasicSQL.sql
+++ Lesson1_BasicSQL/Lesson1_BasicSQL.sql
@@ -1,4 +1,5 @@
-﻿-- ### LIMITS
+﻿--Udacity Queries Practice Lesson 1  Basic SQL
+
 
 --1.16 Quiz: LIMIT
 -- ELECT occurred_at, account_id, channel
@@ -6,7 +7,25 @@
 -- LIMIT 15;
 
 -- 1.18 ORDER BY
-SELECT *
+-- SELECT *
+-- FROM orders
+-- ORDER BY occured_at
+-- LIMIT 155
+
+--1.19 Quiz: ORDER BY
+--1.Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
 FROM orders
-ORDER BY occured_at
-LIMIT 155
+ORDER BY orders
+LIMIT 10;
+
+--Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 5;
+-- Write a query to return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 20;

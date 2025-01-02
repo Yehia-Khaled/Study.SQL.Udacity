ebcee98c6ee9234caa6be7e81a11864c3a56ef07
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2025-01-02 13:44:36 +0200

    1.18 ORDER BY

--- Lesson1_BasicSQL/Lesson1_BasicSQL.sql
+++ Lesson1_BasicSQL/Lesson1_BasicSQL.sql
@@ -1,4 +1,12 @@
-﻿--1.16 Quiz: LIMIT
-SELECT occurred_at, account_id, channel
-FROM web_events
-LIMIT 15;
+﻿-- ### LIMITS
+
+--1.16 Quiz: LIMIT
+-- ELECT occurred_at, account_id, channel
+-- FROM web_events
+-- LIMIT 15;
+
+-- 1.18 ORDER BY
+SELECT *
+FROM orders
+ORDER BY occured_at
+LIMIT 155

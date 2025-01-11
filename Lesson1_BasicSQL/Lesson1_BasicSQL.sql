357d8634f1e79e2a170a7e1148f333d40e2b7c78
Yehia-Khaled <yehiakhaled28@gmail.com>
2025-01-11 15:42:28 +0200

    1.21 ORDER BY Part II

--- Lesson1_BasicSQL/Lesson1_BasicSQL.sql
+++ Lesson1_BasicSQL/Lesson1_BasicSQL.sql
@@ -13,19 +13,24 @@
 -- LIMIT 155
 
 --1.19 Quiz: ORDER BY
---1.Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
-SELECT id, occurred_at, total_amt_usd
-FROM orders
-ORDER BY orders
-LIMIT 10;
+----1.Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+--SELECT id, occurred_at, total_amt_usd
+--FROM orders
+--ORDER BY orders
+--LIMIT 10;
 
---Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
-SELECT id, occurred_at, total_amt_usd
+----Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+--SELECT id, occurred_at, total_amt_usd
+--FROM orders
+--ORDER BY total_amt_usd DESC
+--LIMIT 5;
+---- Write a query to return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+--SELECT id, occurred_at, total_amt_usd
+--FROM orders
+--ORDER BY total_amt_usd 
+--LIMIT 20;
+
+-- 1.21 ORDER BY Part II
+SELECT *
 FROM orders
-ORDER BY total_amt_usd DESC
-LIMIT 5;
--- Write a query to return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
-SELECT id, occurred_at, total_amt_usd
-FROM orders
-ORDER BY total_amt_usd 
-LIMIT 20;
+LIMIT 10;
